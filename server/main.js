8d037e92f53133773a176b8fcd27fbb2e6f4c569
Thomas Doki-Thonon <dokithonon@gmail.com>
2024-10-14 17:49:50 +0200

    udpat

--- server/main.js
+++ server/main.js
@@ -6,11 +6,17 @@ Meteor.startup(async function() {
   await Books.removeAsync({});
   await Books.insertAsync({
     title: 'title1',
+    author: 'a',
+    copies: 10,
   })
   await Books.insertAsync({
     title: 'title2',
+    author: 'b',
+    copies: 50,
   })
   await Books.insertAsync({
     title: 'title3',
+    author: 'c',
+    copies: 100,
   })
 });
